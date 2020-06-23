698b4e18f1bb471ec9e2cbbd381c4619dbac9580
akheelysally@gmail.com <akheelysally@gmail.com>
2020-06-23 20:33:52 +0500

    Updates

---
+++ precache-manifest.2e385dcbd33fa323e1f2ea6fa5544081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a006b01c8a928435ec32af1ec501d7c",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "5726ee0e71661c885148",
+    "url": "/simple-todo-app/static/css/main.e75a9df0.chunk.css"
+  },
+  {
+    "revision": "e1c993e3070ff6d419bc",
+    "url": "/simple-todo-app/static/js/2.aadc151e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.aadc151e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5726ee0e71661c885148",
+    "url": "/simple-todo-app/static/js/main.45dd144a.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
